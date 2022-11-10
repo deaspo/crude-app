c07c2f21fa3f06ea3acb1d814ee65f4f5eb7584b
Polycarp Okock <deaspo@users.noreply.github.com>
2022-11-10 09:33:46 +0100

    - Routes updated with more paths

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { BookingsPage, FaqPage, HomePage } from "components/pages";
+import { BookingsPage, FaqPage, HomePage, LocationsPage, LocationPage } from "components/pages";
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/faq' key='route-faq' element={<FaqPage/>}/>
-                <Route path='/list' key='route-list' element={<BookingsPage/>}/>
-                <Route path='/' key='route-default' element={<HomePage/>}/>
+                <Route index element={<HomePage/>}/>
+                <Route path='faq' key='route-faq' element={<FaqPage/>}/>
+                <Route path='list' key='route-list'>
+                    <Route index element={<BookingsPage/>}/>
+                    <Route path=":bookingId" element={<BookingsPage />} />
+                </Route>
+                <Route path='locations'>
+                    <Route index element={<LocationsPage/>}/>
+                    <Route path=":locationId" element={<LocationPage />} />
+                </Route>
                 <Route path="*" element={<Navigate to="/"/>}/>
             </Routes>
         </BrowserRouter>
