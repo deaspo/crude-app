24f238164753387bf4047eed2d429dd9caff012f
Polycarp Okock <deaspo@users.noreply.github.com>
2022-11-10 10:34:40 +0100

    - Refactored booking-list.tsx

--- src/components/bookings-list/booking-list.tsx
+++ src/components/bookings-list/booking-list.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { IconButton, List, ListItem, Typography } from '@mui/material';
+import { IconButton, List, ListItem, ListItemText, Typography } from '@mui/material';
 
 import { useAppSelector } from "redux-tools/hooks";
-import { BookingProps, bookings } from "features/bookings/bookingsSplice";
+import { BookingProps, selectAllBookings, getBookingsStatus } from "features/bookings/bookingsSplice";
 
 import { BookingLocation } from "components/booking-locations";
 import { TimeAgo } from "components/time-ago";
@@ -25,58 +25,68 @@ import {
 import { BookingListProps } from "./booking-list-props";
 
 export const BookingsList = ({handleClickMore}: BookingListProps) => {
-    const bookingsList: BookingProps[] = useAppSelector(bookings);
+    const bookingsList: BookingProps[] = useAppSelector(selectAllBookings);
     const orderedBookings: BookingProps[] = bookingsList.slice().sort((a, b) => b.postedDate.localeCompare(a.postedDate));
     
-    const renderedRowItem = orderedBookings.map((booking, index) => {
-        return (
-            <ListItem sx={{display: "flex"}} key={index} divider disableGutters>
-                <ItemRoot className={listClasses.root} data-testid="item-root">
-                    <ItemBody className={listClasses.body} data-testid="item-body">
-                        <ItemPhoto className={listClasses.photo} data-testid="item-photo">
-                            <img src={"logo192.png"} height={"80px"} width={"80px"} alt={"item-profile"}/>
-                        </ItemPhoto>
-                        <ItemMain className={listClasses.main} data-testid="item-main">
-                            <ItemContent className={listClasses.content} data-testid="item-contents">
-                                <div data-testid="item-title">
-                                    <Typography variant="h4">
-                                        {booking.bookingTitle}
-                                    </Typography>
-                                </div>
-                                <ItemDateLocation className={listClasses.dateLocation} data-testid="item-date-location">
-                                    <div data-testid="item-date">
-                                        <BookedDate timeStamp={booking.bookingDate}/>
+    let renderedRowItem: JSX.Element[];
+    
+    const bookingsStatus = useAppSelector(getBookingsStatus);
+    
+    if (bookingsStatus === "loading") {
+        renderedRowItem = [<ListItemText key="loading-message">Loading List</ListItemText>]
+    } else if (bookingsStatus === "succeeded") {
+        renderedRowItem = orderedBookings.map((booking) => {
+            return (
+                <ListItem sx={{display: "flex"}} key={booking.id} divider disableGutters>
+                    <ItemRoot className={listClasses.root} data-testid="item-root">
+                        <ItemBody className={listClasses.body} data-testid="item-body">
+                            <ItemPhoto className={listClasses.photo} data-testid="item-photo">
+                                <img src={"logo192.png"} height={"80px"} width={"80px"} alt={"item-profile"}/>
+                            </ItemPhoto>
+                            <ItemMain className={listClasses.main} data-testid="item-main">
+                                <ItemContent className={listClasses.content} data-testid="item-contents">
+                                    <div data-testid="item-title">
+                                        <Typography variant="h4">
+                                            {booking.bookingTitle}
+                                        </Typography>
                                     </div>
-                                    <div data-testid="item-location">
-                                        <BookingLocation locationId={booking.bookingLocationId}/>
+                                    <ItemDateLocation className={listClasses.dateLocation} data-testid="item-date-location">
+                                        <div data-testid="item-date">
+                                            <BookedDate timeStamp={booking.bookingDate}/>
+                                        </div>
+                                        <div data-testid="item-location">
+                                            <BookingLocation locationId={booking.bookingLocationId}/>
+                                        </div>
+                                        <div data-testid="item-location">
+                                            <span> Hours Booked: {booking.bookedHours}</span>
+                                        </div>
+                                    </ItemDateLocation>
+                                    <div data-testid="item-posted-duration">
+                                        <TimeAgo timeStamp={booking.postedDate}/>
                                     </div>
-                                    <div data-testid="item-location">
-                                        <span> Hours Booked: {booking.bookedHours}</span>
-                                    </div>
-                                </ItemDateLocation>
-                                <div data-testid="item-posted-duration">
-                                    <TimeAgo timeStamp={booking.postedDate}/>
-                                </div>
-                                <ReactionButton booking={booking}/>
-                            </ItemContent>
-                            <div data-testid="item-price"><BookingPrice value={booking.bookingPrice}/></div>
-                        </ItemMain>
-                    </ItemBody>
-                    <ItemMore className={listClasses.more} data-testid="item-more-option">
-                        <IconButton
-                            size="small"
-                            edge='end'
-                            onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
-                                handleClickMore(event, booking);
-                            }}
-                        >
-                            <MoreVertIcon/>
-                        </IconButton>
-                    </ItemMore>
-                </ItemRoot>
-            </ListItem>
-        )
-    });
+                                    <ReactionButton booking={booking}/>
+                                </ItemContent>
+                                <div data-testid="item-price"><BookingPrice value={booking.bookingPrice}/></div>
+                            </ItemMain>
+                        </ItemBody>
+                        <ItemMore className={listClasses.more} data-testid="item-more-option">
+                            <IconButton
+                                size="small"
+                                edge='end'
+                                onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+                                    handleClickMore(event, booking);
+                                }}
+                            >
+                                <MoreVertIcon/>
+                            </IconButton>
+                        </ItemMore>
+                    </ItemRoot>
+                </ListItem>
+            )
+        });
+    } else {
+        renderedRowItem = [<ListItemText key="error-message">Error</ListItemText>]
+    }
     
     return (
         <List sx={{
