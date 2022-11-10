ee5a9cf31dcb6408e43f306da856e3844ebcb6ff
Polycarp Okock <deaspo@users.noreply.github.com>
2022-11-10 08:53:23 +0100

    - Clean up of locationsSplice.tsx - function to mock getting data from server

--- src/features/locations/locationsSplice.tsx
+++ src/features/locations/locationsSplice.tsx
@@ -1,23 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "redux-tools/store";
 
-interface LocationState {
+export interface LocationProps {
     id: string,
     location: string
 }
 
-const initialState: LocationState[] = [
+const initialState: LocationProps[] = [
     {id: '0', location: 'Bratislava'},
     {id: '1', location: 'Vienna'},
     {id: '2', location: 'Prague'}
 ]
 
+// Mock Server call functions
+function mockGetLocationData(): Promise<{data:LocationProps[]}> {
+    return new Promise<{data:LocationProps[]}>((resolve) => {
+        resolve({data:[]})
+    })
+}
+
+export const fetchLocations = createAsyncThunk(
+    '/locations/fetchLocations',
+    async () => {
+        const response = await mockGetLocationData();
+        return response.data
+})
+
 export const locationsSplice = createSlice({
     name: 'locations',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchLocations.fulfilled,(state, action) => {
+            return action.payload;
+        })
+    }
 });
 
 export const allLocations = (state: RootState) => state.locations;
+
+export const selectLocationsById = (state: RootState, locationId: string | undefined) => state.locations.find(location => location.id === locationId);
 
 export default locationsSplice.reducer;
