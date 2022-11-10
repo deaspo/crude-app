e6dd18a2246b84981e00826401276891aa81a9a7
Polycarp Okock <deaspo@users.noreply.github.com>
2022-11-10 09:31:00 +0100

    - useEffect to fetch bookings on app load, prevent missing data when page is reloaded

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Router } from "components/router";
+import { useAppDispatch, useAppSelector } from "redux-tools";
+import { fetchBookings, getBookingsStatus } from "features";
 
 export const App = () => {
+    const dispatch = useAppDispatch();
+    const bookingsStatus = useAppSelector(getBookingsStatus);
+    
+    useEffect(() => {
+        if (bookingsStatus === 'idle') {
+            dispatch(fetchBookings())
+        }
+    }, [bookingsStatus,dispatch]);
+    
     return (
         <div className="min-h-full">
             <main>
