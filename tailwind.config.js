27c5d2b0844187837abd81b21ad476744e83a92e
Polycarp Okock <deaspo@users.noreply.github.com>
2023-02-07 14:54:57 +0100

    - updated the tailwind.config.js with info about the tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
-
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx, html}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-elements/dist/plugin")],
+};
 // Run this command after setting up the ./src/assets/tailwind.css and main.css
 // npx tailwindcss -i ./src/assets/tailwind.css -o ./src/assets/main.css --watch
+// Extedn functionality of tailwind - https://tailwind-elements.com/quick-start/
