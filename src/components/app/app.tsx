cba49aea685e942a61ea7e69ebd5626efff3390c
Polycarp Okock <deaspo@users.noreply.github.com>
2023-02-07 14:33:53 +0100

    - fetch all location info on app load

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,20 @@
 import React, {useEffect} from 'react';
 import { Router } from "components/router";
-import { useAppDispatch } from "redux-tools";
-import { extendedApiSlice } from "features";
+import { useAppDispatch, useAppSelector } from "redux-tools";
+import { extendedApiSlice, fetchLocations, getLocationsStatus } from "features";
 
 export const App = () => {
     const dispatch = useAppDispatch();
-    
+    const locationsStatus = useAppSelector(getLocationsStatus);
     useEffect(() => {
         dispatch(extendedApiSlice.endpoints.getBookings.initiate());
     }, [dispatch]);
+    
+    useEffect(() => {
+        if (locationsStatus === 'idle') {
+            dispatch(fetchLocations());
+        }
+    }, [locationsStatus, dispatch])
     
     
     return (
